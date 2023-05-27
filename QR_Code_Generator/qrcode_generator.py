f0ae95b947853b9c189404dbf2cddcf8d0197f12
Soorjyakanta Sethy <99492724+soorjya@users.noreply.github.com>
2023-05-27 11:42:31 +0530

    Update qrcode_generator.py

--- QR_Code_Generator/qrcode_generator.py
+++ QR_Code_Generator/qrcode_generator.py
@@ -13,6 +13,39 @@ def generate_qrcode(text):
     qr.make(fit=True)
     img = qr.make_image(fill_color="black", back_color="white")
     img.save("qrimg001.png")
-    
+
+def generate_contact_qr(name, email, phone):
+    # Create vCard string
+    vcard = f"BEGIN:VCARD\nVERSION:3.0\nN:{name}\nEMAIL:{email}\nTEL:{phone}\nEND:VCARD"
+    # Generate QR code
+    qr = qrcode.QRCode(box_size=10)
+    qr.add_data(vcard)
+    qr.make(fit=True)
+    qr_img = qr.make_image(fill_color="black", back_color="white")
+    return qr_img
+
+def generate_wifi_qr(ssid, password):
+    # Create Wi-Fi network string
+    wifi = f"WIFI:T:WPA;S:{ssid};P:{password};;"
+    # Generate QR code
+    qr = qrcode.QRCode(box_size=10)
+    qr.add_data(wifi)
+    qr.make(fit=True)
+    qr_img = qr.make_image(fill_color="black", back_color="white")
+    return qr_img
+
+# Example usage for contact information
+contact_name = "John Doe"
+contact_email = "johndoe@example.com"
+contact_phone = "+123456789"
+contact_qr = generate_contact_qr(contact_name, contact_email, contact_phone)
+contact_qr.save("contact_qr.png")
+
+# Example usage for Wi-Fi credentials
+wifi_ssid = "MyWiFiNetwork"
+wifi_password = "MyPassword123"
+wifi_qr = generate_wifi_qr(wifi_ssid, wifi_password)
+wifi_qr.save("wifi_qr.png")    
+
 url = input("Enter your url: ")
 generate_qrcode(url)
